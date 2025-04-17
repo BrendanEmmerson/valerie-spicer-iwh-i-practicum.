78b3bf0bcdeaed0409b38d0b572463a6cc328b65
Brendan Emmerson <emmersonbrendan@gmail.com>
2025-04-17 13:05:38 +0000

    Update 4 - addition of key update to readme and update to index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -8,11 +9,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.get("/update-cobj", (req, res) => {
-    res.render("updates", { title: "Update Custom Object Form | Integrating With HubSpot I Practicum" });
-  });
+
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
@@ -45,7 +45,7 @@ app.get("/", async (req, res) => {
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 app.get("/update-cobj", (req, res) => {
-    res.render("updates");
+    res.render("updates", { title: "Update Custom Object Form | Integrating With HubSpot I Practicum" });
   });
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
